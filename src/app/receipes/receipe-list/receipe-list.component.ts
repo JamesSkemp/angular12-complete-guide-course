27860f892c87cc55ee38635cbf8e55e9a4a7394f
James Skemp <strivinglife@gmail.com>
2021-07-11 17:01:06 -0500

    Add receipe class, dummy data, and list display

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Receipe } from '../receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipe-list.component.scss']
 })
 export class ReceipeListComponent implements OnInit {
+  receipes: Receipe[] = [
+    new Receipe('A Test Receipe', 'This is test 1.', 'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d'),
+    new Receipe('A 2nd Test Receipe', 'This is test 2.', 'https://c.pxhere.com/photos/33/0f/meat_vegetables_gemuesepiess_mushrooms_meat_skewer_barbecue_summer_benefit_from-610407.jpg!d')
+
+
+
+  ];
 
   constructor() { }
 
