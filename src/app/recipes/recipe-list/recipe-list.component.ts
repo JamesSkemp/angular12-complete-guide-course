0935d063b8b27411d34664194cc3105fd6e0defc
James Skemp <strivinglife@gmail.com>
2021-07-12 12:32:58 -0500

    Add functionality to support showing details for a selected recipe
    This isn't the best way to handle it, as nested components need to emit up multiple times.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +8,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() listRecipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is test 1.', 'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d'),
     new Recipe('A 2nd Test Recipe', 'This is test 2.', 'https://c.pxhere.com/photos/33/0f/meat_vegetables_gemuesepiess_mushrooms_meat_skewer_barbecue_summer_benefit_from-610407.jpg!d')
@@ -20,4 +23,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.listRecipeSelected.emit(recipe);
+  }
 }
